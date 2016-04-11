e95109801877edc23e8bd281e5444788a6c69bb5
stephane <contact@depoilly.com>
2016-04-11 17:06:51 +0200

    Initial Commit

---
+++ MegaForce4Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MegaForce4Fun
+//
+//  Created by Stéphane DEPOILLY on 11/04/2016.
+//  Copyright © 2016 Stéphane DEPOILLY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
